2ddc0018d5ae44b1a3df380833384c0ae3a3d0c6
Ryan Safa <safarzadehryan@gmail.com>
2024-04-13 21:43:56 -0700

    remove working

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -7,10 +7,27 @@ export default class make_UI {
     // create a new project button and switch to this project on clicking 
     addProject(project) {
         const projectContainer = document.querySelector('.project-container');
+        const btnWrapper = document.createElement('div');
+
+        btnWrapper.classList.add('btn-wrapper');
+
         const newProject = document.createElement('button');
+        const removeBtn = document.createElement('button');
+        
+        removeBtn.textContent = 'X';
+        removeBtn.classList.add('proj-remove-btn');
+        removeBtn.style.background = 'pink';
+        removeBtn.addEventListener('click', () => {
+            this.removeProject(project);
+        });
+
         newProject.textContent = project.getName();
         newProject.classList.add('proj-button');
-        projectContainer.appendChild(newProject);
+
+        btnWrapper.appendChild(newProject);
+        btnWrapper.appendChild(removeBtn);
+
+        projectContainer.appendChild(btnWrapper);
         console.log(project.getName());
         newProject.addEventListener('click', () => {
             document.querySelector('.task-page').innerHTML = '';
@@ -24,6 +41,13 @@ export default class make_UI {
         for (let project of projects) {
             if (project.getName() === 'Today' || project.getName() === 'This Week') continue;
             const btn = document.createElement('button');
+            const removeBtn = document.createElement('button');
+            removeBtn.addEventListener('click', () => {
+                this.removeProject(project);
+            });
+            removeBtn.textContent = 'X';
+            removeBtn.classList.add('proj-remove-btn');
+            btn.appendChild(removeBtn);
             btn.textContent = project.getName();
             btn.style.width = '100%';
             btn.addEventListener('click', () => {
@@ -48,5 +72,18 @@ export default class make_UI {
         console.log(newProject);
         document.querySelector('.task-page').innerHTML = '';
         this.loadTasks(newProject);
+    }
+
+    removeProject(toRemove) {
+        for (const project of document.querySelector('.project-container').querySelectorAll('.btn-wrapper')) {
+            const projButton = project.querySelector('.proj-button');
+            console.log(projButton.parentNode);
+            console.log(`${projButton.textContent} === ${toRemove.getName()}`);
+            if (projButton.textContent === toRemove.getName()) {
+                document.querySelector('.project-container').removeChild(projButton.parentNode);
+                console.log(projButton.parentNode);
+                break;
+            }
+        }
     }
 };
